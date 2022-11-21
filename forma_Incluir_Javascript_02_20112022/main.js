746cbb132425fc5063f41e53f4e8296e674b50f4
Senawguevara <wguevara90@misena.edu.co>
2022-11-20 21:33:48 -0500

    forma_Incluir_Javascript_02

---
+++ forma_Incluir_Javascript_02_20112022/main.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded", (e) => {
+    alert('Conexión con el archivo JavaScript Exitoso');
+
+    document.querySelector("body").innerHTML='<h1>Ver la consola del navegador o oprima la tecla F12 para ver los resultados</h1>';
+    console.log('Conexión con el archivo JavaScript Exitoso' );
+    
+    
+    })
